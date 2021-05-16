7b2ff12df055dc67588f2b3328392023af29f6b0
DAZHI Zhang <dazhi.zhang@scotiabank.com>
2021-05-16 01:04:23 -0400

    parent dbcaaf8db7e9535a6670e073850c17ba09766ec6 author DAZHI Zhang <dazhi.zhang@scotiabank.com> 1621141463 -0400 committer DAZHI Zhang <dazhi.zhang@scotiabank.com> 1621544573 -0400
    parent dbcaaf8db7e9535a6670e073850c17ba09766ec6
    author DAZHI Zhang <dazhi.zhang@scotiabank.com> 1621141463 -0400
    committer DAZHI Zhang <dazhi.zhang@scotiabank.com> 1621544492 -0400
    
    Adding solutions
    
    leetcode 206 reverse list
    
    Add leetcode submit report for 21
    
    Add basic backtrace for 46_permutations

--- python/list/leetcode/206_reverse_linked_list.py
+++ python/list/leetcode/206_reverse_linked_list.py
@@ -4,40 +4,58 @@ import os
 sys.path.append(os.getcwd())
 
 
-from list.singly_node import SinglyNode
+from list.list_node import ListNode
 
-def initSinglyList(length: int) -> SinglyNode:
-  head = SinglyNode(0, None)
+def initSinglyList(length: int) -> ListNode:
+  head = ListNode(0, None)
   current = head
 
   i = 0
   while (i < length):
-    i = i + 1
-    temp = SinglyNode(i, None)
+    i += 1
+    temp = ListNode(i, None)
     current.next = temp
     current = temp
 
   return head
 
 
-def show(head: SinglyNode):
+def show(head: ListNode):
   if head is None:
     return
     
   i = 0
   while (head.next is not None):
-    print ("#%d is %d"%(i, head.data))
+    print ("#%d is %d"%(i, head.val))
     head = head.next
-    i = i + 1
+    i += 1
 
-def reverse(head: SinglyNode) -> SinglyNode:
+"""
+Runtime: 24 ms, faster than 99.17% of Python3 online submissions for Reverse Linked List.
+Memory Usage: 15.7 MB, less than 42.36% of Python3 online submissions for Reverse Linked List.
+"""
+def reverseSingle(head: ListNode) -> ListNode:
+  prev, curr, next = None, head, None
+
+  while (curr is not None):
+    next = curr.next
+    curr.next = prev
+    prev = curr
+    curr = next
+  
+  return prev 
+
+"""
+Runtime: 36 ms, faster than 61.62% of Python3 online submissions for Reverse Linked List.
+Memory Usage: 15.7 MB, less than 42.36% of Python3 online submissions for Reverse Linked List.
+"""
+def reverseTuple(head: ListNode) -> ListNode:
   prev, curr = None, head
 
   while (curr is not None):
     prev, prev.next, curr = curr, prev, curr.next
 
-  head = prev
-  return head
+  return prev 
 
 def test():
   head = initSinglyList(5)
@@ -45,9 +63,12 @@ def test():
   print("original list")
   show(head)
 
-  print("reversed list")
-  head = reverse(head)
-  show(head)
+  print("reversed list by single assignment")
+  show(reverseSingle(head))
+
+  head = initSinglyList(5)
+  print("reversed list by tuple assignment")
+  show(reverseTuple(head))
 
 if __name__ == "__main__":
   test()
--- python/list/singly_node.py
+++
@@ -1,4 +0,0 @@
-class SinglyNode:
-  def __init__(self, data: int, next=None):
-    self.data = data
-    self.next = next 
